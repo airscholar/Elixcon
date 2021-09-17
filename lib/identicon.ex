29b10bdbd87f71baad3a4d93b75a3784940455be
Yusuf Ganiyu <airscholar@gmail.com>
2021-09-17 22:00:40 +0100

    Finalized with generation of image from the input string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,8 +8,24 @@ defmodule Identicon do
     |> pick_color()
     |> build_grid()
     |> filter_odd_grid()
+    |> build_pixel_map()
+    |> create_image()
+    |> save_image(input)
+  end
 
-    # |> hash_to_color() |> color_to_identicon() |> identicon_to_svg()
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
+  end
+
+  def create_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
   end
 
   def filter_odd_grid(%Identicon.Image{grid: grid} = image) do
@@ -18,8 +34,7 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
@@ -34,12 +49,27 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    px_map =
+      grid
+      |> Enum.map(fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: px_map}
+  end
+
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
   end
 
-  @spec hash_input(any) :: %Identicon.Image{color: nil, hex: [byte], image: nil}
   def hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
